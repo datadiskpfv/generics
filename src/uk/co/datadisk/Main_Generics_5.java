5cf0af1c6f2bb0fe5b14baa3779afe7a62079363
pvalle <paul.valle@orbian.com>
2018-10-03 08:42:42 +0100

    looking at advanced Generics

--- src/uk/co/datadisk/Main_Generics_5.java
+++ src/uk/co/datadisk/Main_Generics_5.java
@@ -1,24 +1,27 @@
 package uk.co.datadisk;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 public class Main_Generics_5 {
 
-    // unbound parameters using <?>
+    // upper and lower bound parameters using <? extends Shape> and <? super Shape>
+    // upper = extends
+    // lower = super
 
     public static void main(String[] args) {
 
-        Shape s1 = new Circle();
-        Shape s2 = new Rectangle();
-        s1.draw();
-        s2.draw();
+        Shape c1 = new Circle();
+        Shape r1 = new Rectangle();
+        Square s1 = new Square();
+        //c1.draw();
+        //r1.draw();
 
-        Collection<Shape> shapeCollection = new ArrayList<>();
-        shapeCollection.add(s1);
-        shapeCollection.add(s2);
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(c1);
+        shapes.add(r1);
 
-        if (s1 instanceof Object) {
+        if (c1 instanceof Object) {
             System.out.println("Shape instance of Object");
         }
 
@@ -30,6 +33,37 @@ public class Main_Generics_5 {
         Object o1 = new Object();
         //shapeCollection.add(o1);          // compile error, you cannot add an Object to a Collection of Shape's, it has to be a Shape object
 
+        producer(shapes);
+        consumer(shapes);
+    }
+
+    // UPPER BOUND
+    private static void producer(List<? extends Shape> shapes) {
+        // Producer: uses the extends keyword and thus you can list but you cannot add to the list
+
+        // Unable to add to the list
+        //Shape c1 = new Circle();
+        //shapes.add(c1);
+
+        // You are free to use any of Shapes methods
+        for (Shape shape : shapes){
+            shape.draw();
+        }
+    }
+
+    // LOWER BOUND
+    private static void consumer(List<? super Shape> shapes){
+        //Consumer: uses the super keyword and thus you can add but you cannot use any methods of the class (only Object methods)
+
+        // You are free to add to the list
+        Shape c1 = new Circle();
+        shapes.add(c1);
+
+        // Here i am cheating as i am using a Object method, but no Shape methods are available because
+        // its of type Object
+        for (Object shape : shapes){
+            System.out.println(shape.toString());
+        }
     }
 }
 
@@ -38,12 +72,33 @@ class Circle implements Shape {
     public void draw() {
         System.out.println("Draw Circle");
     }
+
+    @Override
+    public String toString() {
+        return "I am a Circle";
+    }
 }
 
 class Rectangle implements Shape {
     @Override
     public void draw() {
         System.out.println("Draw Rectangle");
+    }
+
+    @Override
+    public String toString() {
+        return "I am a Rectangle";
+    }
+}
+
+class Square  {
+    public void draw() {
+        System.out.println("Draw Rectangle");
+    }
+
+    @Override
+    public String toString() {
+        return "I am a Square";
     }
 }
 
